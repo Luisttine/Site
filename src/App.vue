a2a732ec871a22548f781cb630ec0e0ec77d4ecc
Luittine <luisttine@gmail.com>
2020-06-11 14:30:15 -0300

    Teste

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <div id="app">
+    <h1> "Faça login para acessar o site!"{{ titulo }}</h1>
+    <Mensage msg="e-mail"/>
+    <input type="text" name="nome" value="" placeholder="e-mail" />
+    <Login msg="Digite seu e-mail!"/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
+  var Login = vue.component( 'Login') {=
+    routes:[
+  var router = new VueRouter({
+    {path:}'/Login', componet: Login}
+    ]
+    )};
+
+var app = new Vue({
+  el: "#app",
+  router,
+  data: {
+    titulo: "Faça seu login!",
+}
+})
+
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, 
+    Login
   }
 }
 </script>
